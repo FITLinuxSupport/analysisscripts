b771464ebbe6f7117cb4ac8acce29f449bee1c19
warrenjeffs <warren.jeffs@stfc.ac.uk>
2015-07-16 14:36:45 +0100

    Create Desktop shortcut to examples

---
+++ create example-shortcuts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#specify your folder you'd like this to run on - this is where you symbolic links will be created.
+for sourcefolder in home
+        do
+
+                #find folders in above folder that are not symbolic links
+                for DIRS in $(find /$sourcefolder/ -maxdepth 1 \! -type l | xargs ls -d|grep -vw '/home/')
+                        do
+                        #get the folder name
+                        FOLDERNAME=$(basename $DIRS)
+                                
+								#Making home dirs
+                                echo "Making Desktop folder for missing accounts"
+                                mkdir /$sourcefolder/$FOLDERNAME/Desktop/
+                                #move the folder
+                                echo "Copying desktop items"
+                                ln -s /home/resources/HoraceExampleScripts/ /$sourcefolder/$FOLDERNAME/Desktop/HoraceExampleScripts
+                                echo " /$sourcefolder/$FOLDERNAME/Desktop/ Done"
+								echo " "
+                        done
+
+                done
+
+
