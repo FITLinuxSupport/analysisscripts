aca6aa20da1408fe2d2b47f3b099f2827d127f9c
warrenjeffs <warren.jeffs@stfc.ac.uk>
2015-08-17 15:54:17 +0100

    tidy up script

--- HoraceExampleScriptsLink.sh
+++ HoraceExampleScriptsLink.sh
@@ -1,1 +1,7 @@
-ln -s /home/resources/HoraceExampleScripts/ ~/Desktop/HoraceExampleScripts
+if [ ! -f  ~/Desktop/HoraceExampleScripts];
+then
+    exit 0
+else
+    ln -s /home/resources/HoraceExampleScripts/ ~/Desktop/HoraceExampleScripts
+fi
+}
